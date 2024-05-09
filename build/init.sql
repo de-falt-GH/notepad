1aa72d6362e52c505800f6eff95ef0056f8beab5
de_falt <virchuklaev@gmail.com>
2024-05-09 22:47:30 +0300

    докер сука где контейнеры?!

---
+++ build/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    login TEXT UNIQUE NOT NULL, 
+    password_hash TEXT NOT NULL, 
+    email TEXT UNIQUE NOT NULL, 
+    name TEXT, 
+    info TEXT
+);
+
+CREATE TABLE note (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES "user",
+    name TEXT NOT NULL, 
+    data TEXT NOT NULL, 
+    public BOOLEAN,
+);
