83de5313f2d45b7eff2cf2ff40432aa2ea0f9287
de_fault <virchuklaev@gmail.com>
2024-05-12 15:57:32 +0300

    просовываем BACKEND_PORT в статический бандл фронта, потому что лучше я не придумал

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,25 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { type UserConfigFn, defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const config: UserConfigFn = ({ mode }) => {
+	const { BACKEND_PORT: backendPortEnvFile } = loadEnv(
+		mode,
+		process.cwd() + '/../build',
+		'',
+	)
+	const { BACKEND_PORT: backendPortFromProcess } = process.env
+
+	const backendPort = backendPortEnvFile ?? backendPortFromProcess
+
+	console.log({ backendPort })
+
+	return defineConfig({
+		plugins: [sveltekit()],
+
+		define: {
+			__BACKEND_PORT__: backendPort,
+		},
+	})
+}
+
+export default config
