64c310a9c401c987525b8df84a5a42407d35c5cc
de_fault <virchuklaev@gmail.com>
2024-05-21 20:20:20 +0300

    fix: forgot to remove SetEnv

--- backend/main.go
+++ backend/main.go
@@ -6,22 +6,11 @@ import (
 	"kursarbeit/storage"
 	common_storage "kursarbeit/storage/common"
 	user_storage "kursarbeit/storage/user"
-	"os"
 
 	"go.uber.org/zap"
 )
 
 func main() {
-	os.Setenv("POSTGRES_HOST", "0.0.0.0")
-	os.Setenv("POSTGRES_PORT", "5432")
-	os.Setenv("POSTGRES_DB", "kursarbeit")
-	os.Setenv("POSTGRES_USER", "postgres")
-	os.Setenv("POSTGRES_PASSWORD", "postgres")
-	os.Setenv("POSTGRES_SSLMODE", "disable")
-
-	os.Setenv("BACKEND_HOST", "127.0.0.1")
-	os.Setenv("BACKEND_PORT", "8080")
-
 	log := zap.Must(zap.NewProduction()).Sugar()
 	defer log.Sync()
 
