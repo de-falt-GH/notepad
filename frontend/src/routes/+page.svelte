f528b4b46e1ae48ee4b5a8c1f9a4a08005954459
de_fault <virchuklaev@gmail.com>
2024-05-21 20:48:39 +0300

    wip: test register/login ui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Button } from '$lib/components/ui/button'
+	import { authToken } from '$lib/auth'
+	import { apiClient } from '$lib/api'
+
+	function onRegister() {
+		apiClient
+			.register({
+				login: 'picalines',
+				password: '12345',
+				email: 'f@off.com',
+				name: 'petya',
+				info: 'not much',
+			})
+			.then(console.log)
+	}
+
+	function onLogin() {
+		apiClient.authenticate('123', '123').then(console.log)
+	}
+</script>
+
+Auth token: {$authToken}
+
+<Button variant="outline" on:click={onRegister}>Register</Button>
+<Button variant="outline" on:click={onLogin}>Login</Button>
